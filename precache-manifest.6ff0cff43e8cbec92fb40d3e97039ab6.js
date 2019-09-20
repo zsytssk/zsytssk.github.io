4620e21a5f89a47413b5584f629eb04fe23d29fb
张世阳 <ZHANGSHIYANG162@pingan.com.cn>
2019-09-20 11:20:29 +0800

    master:update

---
+++ precache-manifest.6ff0cff43e8cbec92fb40d3e97039ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073773762d14b1ef85fbad9865399880",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0c1724a071482089943",
+    "url": "/static/css/main.bd4e9e42.chunk.css"
+  },
+  {
+    "revision": "9bc13d8ae5187d3139cf",
+    "url": "/static/js/2.03c931e6.chunk.js"
+  },
+  {
+    "revision": "b0c1724a071482089943",
+    "url": "/static/js/main.3b9158f3.chunk.js"
+  },
+  {
+    "revision": "69b84c4bc2494c685e83",
+    "url": "/static/js/runtime~main.abc96412.js"
+  }
+]);
