596b03d20779412b653d615ed1daf6b655c681a7
zsytssk <zsytssk@gmail.com>
2022-03-11 17:55:49 +0800

    master:

--- code/test/http.ts
+++ code/test/http.ts
@@ -1,8 +1,9 @@
 // get
-function test1() {
+function xmlTest1() {
   var xhr = new XMLHttpRequest(),
     method = "GET",
-    url = "https://api.publicapis.org/entries";
+    url =
+      "https://api.weatherapi.com/v1/current.json?key=f0ebd22d679b4f44ba494712221103&q=London&aqi=no";
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -15,10 +16,39 @@ function test1() {
 }
 
 // post
-function test2() {
+function xmlTest2() {
+  const new_title = "title";
+  const userId = "zsytssk";
+  const body = `est rerum tempore vitae
+sequi sint nihil reprehenderit dolor beatae ea dolores neque
+fugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis
+qui aperiam non debitis possimus qui neque nisi nulla`;
+
+  var xhr = new XMLHttpRequest(),
+    method = "POST",
+    url = "https://jsonplaceholder.typicode.com/posts";
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 201)) {
+      console.log(xhr.responseText);
+    }
+  };
+  xhr.open(method, url, true);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  xhr.send(
+    JSON.stringify({
+      title: new_title,
+      body: body,
+      userId: userId,
+    })
+  );
+}
+
+// Auth
+function xmlTest3() {
   var xhr = new XMLHttpRequest(),
     method = "GET",
-    url = "https://jsonplaceholder.typicode.com/users";
+    url = "https://auth0.com/";
 
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -27,9 +57,8 @@ function test2() {
   };
 
   xhr.open(method, url, true);
+  xhr.setRequestHeader("Authorization", "Bearer XXXXXXXXXXXXXXXXXXXXX");
   xhr.send();
 }
 
-// Auth
-
-test1();
+xmlTest3();
