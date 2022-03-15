e8bd6079939f292c5e55432d6c12c6a37853f8b3
zsytssk <zsytssk@gmail.com>
2022-03-15 17:03:54 +0800

    master:

--- code/test.ts
+++ code/test.ts
@@ -1,17 +1,61 @@
-// 二叉树两种状态的转化
-function isSymmetric(root: (number | null)[]): boolean {
-  for (let i = 0; i < root.length; i++) {
-    const matchIndex = findMatchIndex(i + 1) - 1;
-    console.log(i, matchIndex, root[i] !== root[matchIndex]);
-    if (root[i] !== root[matchIndex]) {
-      return false;
+function mincostTickets(days: number[], costs: number[]): number {
+  const continueDays: number[] = [];
+  let num = 0;
+  for (let i = 0; i < days.length; i++) {
+    if (i === 0) {
+      num += 1;
+      continue;
+    }
+    if (days[i] - days[i - 1] === 1) {
+      num += 1;
+    } else {
+      continueDays.push(num);
+      num = 1;
+    }
+
+    if (i === days.length - 1) {
+      continueDays.push(num);
     }
   }
-  return true;
+  console.log(continueDays);
+  let result = 0;
+  for (const item of continueDays) {
+    const map = calcNum(item);
+    console.log(map);
+    result += map["num1"] * costs[0];
+    result += map["num7"] * costs[1];
+    result += map["num30"] * costs[2];
+  }
+  console.log(`result: ${result}`);
+  return result;
 }
 
-function findMatchIndex(index: number) {
-  const num = index.toString(2);
-  return index ^ (Math.pow(2, num.length - 1) - 1);
+function convertToRange(days: number[], costs: number[]) {
+  let result = 0;
+
+  console.log(result);
 }
-console.log(`test:>`, isSymmetric([1, 2, 2, null, 3, null, 3]));
+
+type NumMap = {
+  num1: number;
+  num7: number;
+  num30: number;
+};
+function calcNum(num: number, map = { num1: 0, num7: 0, num30: 0 } as NumMap) {
+  if (num < 1 * 4) {
+    map["num1"] += num;
+    return map;
+  }
+  if (num < 7 * 2) {
+    map["num7"] += Math.floor(num / 7);
+    const rest = num % 7;
+    calcNum(rest, map);
+    return map;
+  }
+  map["num30"] += Math.floor(num / 30);
+  const rest = num % 15;
+  calcNum(rest, map);
+  return map;
+}
+
+mincostTickets([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31], [2, 7, 15]);
--- doc/review.md
+++ doc/review.md
@@ -103,8 +103,32 @@ https://leetcode-cn.com/problems/word-transformer-lcci/comments/
 Proxy: https://codesandbox.io/s/es6-proxy-forked-x59wft
 Symbol: https://codesandbox.io/s/vnozc?file=/src/index.js
 
-## end
+## http 请求头包含哪些部分
 
-promise 的执行顺序
+HTTP 消息头允许客户端和服务器通过 request 和 response 传递附加信息。一个请求头由名称（不区分大小写）后跟一个冒号“：”，冒号后跟具体的值（不带换行符）组成。
 
-- 完善简历
+根据不同上下文，可将消息头分为：
+
+General headers: 同时适用于请求和响应消息，但与最终消息主体中传输的数据无关的消息头。
+Request headers: 包含更多有关要获取的资源或客户端本身信息的消息头。
+Response headers: 包含有关响应的补充信息，如其位置或服务器本身（名称和版本等）的消息头。
+
+### General
+
+本地地址 远程地址 状态 方法
+
+### Request headers
+
+网站信息 method path
+
+Accept - accept-encoding - accept-language
+authorization - cookie
+
+浏览器信息 : user-agent + platform + ...
+
+### Response headers
+
+access-control-allow- credentials | headers | method | origin
+content- encoding | type
+cache-control expire
+date
