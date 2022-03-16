b7689e8e476409d87b85b784ee2eee5cd93cf29b
zsytssk <zsytssk@gmail.com>
2022-03-16 15:08:02 +0800

    master:

--- code/test.ts
+++ code/test.ts
@@ -1,52 +1,18 @@
-function mincostTickets(days: number[], costs: number[]): number {
-  let result = 0;
-  let num = 0;
-
-  const num1 = Math.ceil(costs[1] / costs[0]) - 1;
-  const num2 = (Math.ceil(costs[2] / costs[1]) - 1) * 7;
-
-  console.log(num1, num2);
-  let start = 0;
-  let current = 0;
-  let bestMatch = 0;
-  for (let i = 0; i < days.length; i++) {
-    num++;
-    current = days[i];
-    const space = current - start + 1;
-
-    if (i === 0) {
-      start = days[0];
-      bestMatch = costs[0];
+function networkDelayTime(times: number[][], n: number, k: number): number {
+  const completeArr = [];
+  const splitArr = times.filter((item) => item[0] !== k);
+  const toArr = [];
+  for (let i = 0; i < n; i++) {
+    const [ui, vi, t] = times[i];
+    let item = t;
+    if (ui !== k) {
       continue;
     }
-
-    if (space >= 30) {
-      result += bestMatch;
-      num = 1;
-      start = current;
-      bestMatch = costs[0];
-    } else if (space >= 7) {
-      result += bestMatch;
-      num = 1;
-      start = current;
-      bestMatch = costs[0];
-    } else if (space < 7 && num > num1) {
-      bestMatch = costs[1];
-    } else if (space < 30 && num > num2) {
-      bestMatch = costs[2];
-    } else {
-      bestMatch += costs[0];
+    const result = networkDelayTime(splitArr, n - 1, vi);
+    if (item === -1) {
+      continue;
     }
-
-    if (i === days.length - 1) {
-      result += bestMatch;
-    }
-
-    console.log(`test:>`, current, space, bestMatch, result);
+    item += result;
+    completeArr.push(vi);
   }
-
-  return result;
 }
-
-const result = mincostTickets([1, 4, 6, 7, 8, 20], [2, 7, 15]);
-console.log(result);
