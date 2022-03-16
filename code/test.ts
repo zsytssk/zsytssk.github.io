535018545947ed4a62749eea02e357b745c8aa48
zsytssk <zsytssk@gmail.com>
2022-03-16 17:06:37 +0800

    master:

--- code/test.ts
+++ code/test.ts
@@ -1,18 +1,42 @@
-function networkDelayTime(times: number[][], n: number, k: number): number {
-  const completeArr = [];
-  const splitArr = times.filter((item) => item[0] !== k);
-  const toArr = [];
-  for (let i = 0; i < n; i++) {
-    const [ui, vi, t] = times[i];
-    let item = t;
-    if (ui !== k) {
-      continue;
+let completeMap: Map<number, number> = new Map();
+function networkDelayTime(times: number[][], n: number, k: number) {
+  completeMap.clear();
+
+  findMatch(times, k);
+  completeMap.delete(k);
+  if (completeMap.size >= n - 1) {
+    return Math.max(...completeMap.values());
+  }
+  return -1;
+}
+
+function findMatch(times: number[][], k: number, step = 0) {
+  const StartArr = times.filter((item) => item[0] === k);
+  const restArr = times.filter((item) => item[0] !== k);
+
+  console.log(`test:>`, k);
+  for (const item of StartArr) {
+    const [, vi, t] = item;
+
+    let curStep = t + step;
+    const saved = completeMap.get(vi);
+    if (saved && saved < curStep) {
+      curStep = saved;
+    } else {
+      completeMap.set(vi, curStep);
     }
-    const result = networkDelayTime(splitArr, n - 1, vi);
-    if (item === -1) {
-      continue;
-    }
-    item += result;
-    completeArr.push(vi);
+    findMatch(restArr, vi, curStep);
   }
 }
+
+const result = networkDelayTime(
+  [
+    [1, 2, 1],
+    [2, 3, 2],
+    [1, 3, 2],
+  ],
+  3,
+  1
+);
+
+console.log(`test:>result`, result);
